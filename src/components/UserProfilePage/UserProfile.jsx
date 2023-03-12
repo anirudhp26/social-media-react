a23968d010dc2ef73c5a7b95a9b9efa99d6b9b89
Anirudh Patel <anirudh26p@gmail.com>
2023-03-12 18:59:39 +0530

    .

--- src/components/UserProfilePage/UserProfile.jsx
+++ src/components/UserProfilePage/UserProfile.jsx
@@ -3,16 +3,32 @@ import { useParams } from "react-router-dom";
 import Axios from "axios";
 import Header from "../Header/Header";
 export default function UserProfile() {
+    const [userinfo, setUserinfo] = React.useState({});
     let { username } = useParams();
     // React.useEffect(() => {
-    //     Axios.get("https://backend-sm.vercel.app/getUserinfo").then((responce) => {
-    //         setLoggedUser(responce.data);
+    //     Axios.post("https://backend-sm.vercel.app/getUserinfo").then((responce) => {
+    //         setUserinfo(responce.data);
     //     })
-    // }, [loggeduser]);
+    // }, []);
     return(
         <>
-        <Header />
+            <Header />
             <p>Profile page for {username}</p>
+            <div className="profile-root">
+                <div className="psec-1">
+                    <i className="fa-regular fa-user"></i>
+                    <div className="psec1-userinfo">
+                        <p>{username}</p>
+                        <p>Full Name</p>
+                        <div className="connectionInfo">
+                            <p>Following</p>
+                            <p>Followers</p>
+                        </div>
+                        <button>Follow/Unfollow</button>
+                    </div>
+                </div>
+                <div className="psec-2"></div>
+            </div>
         </>
     )
 }
